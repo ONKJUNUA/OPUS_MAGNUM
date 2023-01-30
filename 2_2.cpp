207e88afab03d705be13593f29367011c9ad6c48
ONKJUNUA <106984734+XiRO0O@users.noreply.github.com>
2023-01-30 11:20:27 +0100

    4th program

---
+++ 2_2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int ile;
+cout << "\nIle gwiazdek ma miec kapitan?: ";
+cin >> ile;
+cout << "No to narysujmy wszystkie " << ile << ": ";
+
+while(ile)
+{
+if (ile<0)
+    cout << "-";
+cout << "*";
+if (ile>0)
+    ile = ile - 1;
+else ile = ile + 1;
+}
+
+cout << "\nTeraz zmienna ile ma wartosc " << ile << ".";
+}
