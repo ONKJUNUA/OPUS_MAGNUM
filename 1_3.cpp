5e0f5c8aacfbeea46d742e3e08715f8ca68abf68
ONKJUNUA <106984734+XiRO0O@users.noreply.github.com>
2023-01-30 10:55:28 +0100

    cwiczenia1

---
+++ 1_3.cpp
@@ -0,0 +1,11 @@
+//Cwiczenia nr 1
+#include <iostream>
+using namespace std;
+int main () {
+    double cal;
+    cout << "witamy\nna\np\no\nkladzie";
+    cout << "\nPodaj ile cali: ";
+    cin >> cal;
+    cout << "Tyle cm: " << cal*2.54;
+    return 0;
+}
