0ae56618e6d9e71ff29721236c8ce9e8cd410ca8
ONKJUNUA <106984734+XiRO0O@users.noreply.github.com>
2023-02-02 13:40:56 +0100

    35.1 program

---
+++ 6_14.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+constexpr double pi = 3.14159265; 
+constexpr double promien_orb_marsa = 228;
+constexpr double poziom_zanurzenia = -333;
+
+constexpr long long silnia(int n)
+{
+    return n > 0 ? n * silnia(n - 1) : 1;
+}
+
+constexpr double dwa_pi_r(const double & promien) 
+{
+    return 2 * pi* promien ;
+}
+
+constexpr double mile_na_kilometry(double mil) 
+{
+    return (1.609344 * mil);
+}
+
+constexpr int porownaj(char a, char b)
+{
+    return a < b ? -1 : (a == b) ? 0 : +1;
+}
+
+int main()
+{
+    constexpr double buty_kilometrowe = mile_na_kilometry(7); 
+    cout << "7-milowe buty to inaczej buty " << buty_kilometrowe << "-kilometrowe" << endl;
+    constexpr double dystans_mile = 2614;
+    constexpr double dystans_km = mile_na_kilometry(dystans_mile); 
+    cout << "Odleglosc Paryz - Dakar to " << dystans_km << " kilometrow" << endl;
+    for(int m = 100 ; m <= 600 ; m+= 100)
+    {
+        cout << " " << m << " mil odpowiada "
+            << mile_na_kilometry(m) << " km." <<endl; 
+    }
+    constexpr int r = porownaj('a', 'a');
+    switch (r)
+    {
+        case 1: cout << "Pierwszy wiekszy od drugiego" << endl; break;
+        case 0: cout << "Sa rowne" << endl; break;
+        case -1: cout << "Pierwszy mniejszy od drugiego" << endl; break;
+    }
+    
+    constexpr double dl_orbity_marsa = dwa_pi_r(promien_orb_marsa);
+    cout << "Obwod orbiry Marsa: " << dl_orbity_marsa << endl;
+    
+    constexpr long long silnia14 = silnia(14);
+    cout << "Silnia: 14! to " << silnia14 << endl;
+}
